ae989e5242316bead74d71cd4ac2c8799c55cd25
acedon <acedon@mi.fu-berlin.de>
2019-11-05 14:03:11 +0000

    Replace aufgabe2.cpp

--- aufgabe2/aufgabe2.cpp
+++ aufgabe2/aufgabe2.cpp
@@ -5,6 +5,9 @@
 #include "aufgabe2.hpp"
 
 int comp(const std::string pat, const std::string text, int pos){
+    //Funktion die zwei Strings an der durch Pos angegebenen Stelle Gleichen oder nicht
+    //Bei gleichheit wird 0, bei pat<text wird -1 und bei pat>taxt 1 zuückgegeben    
+
     int out = 0;
      //if(pat[0]==text[pos]){  
     int j = 0; 
@@ -25,6 +28,38 @@ int comp(const std::string pat, const std::string text, int pos){
 
 }
 
+bool wholeComp(const std::string pat, const std::string text, int x, int y){
+    
+    //Funktion die zurück gibt ob das gesuchte pattern bzw Substr 1 lex. kleiner ist als Text      bzw Substr2
+    // Falls x != 0 wird im Str2 erst ab stelle x gesucht
+    if(x==0 && y==0){
+        for(int i = 0; i < pat.size();++i){
+            if(pat[i]<=text[i]){
+                if(pat[i]==text[i]){
+                    continue;
+                }else{
+                    return true;
+                }   
+            }else{
+                return false;
+            }
+        }
+    }else{
+        for(int i = 0; i<pat.size();++i){
+            if(pat[i+x]<=text[(y+i)]){
+                if(pat[i+x]==text[(y+i)]){
+                    continue;
+                }else{
+                    return true;
+                }
+            }else{
+                return false;
+            }
+        }
+    }
+
+}
+
 void construct(std::vector<uint32_t>& sa, const std::string& text){
         if(text == ""){
         	std::cout << "Im Leeren Text können wir nichts finden" << std::endl;
@@ -37,7 +72,7 @@ void construct(std::vector<uint32_t>& sa, const std::string& text){
 		size_t pos = 0;
 		std::generate(sa.begin(), sa.end(), [&pos]() { return pos++; });
 		std::sort(sa.begin(), sa.end(), [text](uint32_t x, uint32_t y) {
-			return text.substr(x) < text.substr(y);
+			return wholeComp(text,text,x,y);
 		});
 
 }

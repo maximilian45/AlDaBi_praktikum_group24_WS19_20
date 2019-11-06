ace1e989ed0527d2d5c4313718619f2cd84f7772
acedon <acedon@mi.fu-berlin.de>
2019-11-06 18:23:56 +0000

    Replace aufgabe2.cpp

--- aufgabe2/aufgabe2.cpp
+++ aufgabe2/aufgabe2.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include "aufgabe2.hpp"
 
-/*
 int comp(const std::string pat, const std::string text, int pos){
     //Funktion die zwei Strings an der durch Pos angegebenen Stelle Gleichen oder nicht
     //Bei gleichheit wird 0, bei pat<text wird -1 und bei pat>taxt 1 zuückgegeben    
@@ -27,17 +26,26 @@ int comp(const std::string pat, const std::string text, int pos){
     }
     return 0;
 
-} */
+}
 
-bool wholeComp(const std::string pat, const std::string text, int x, int y){   //bitte nur auf den Text referenzieren, um bei großen strings nicht aus dem speicher zu fliessen...
+bool wholeComp(const std::string pat, const std::string text, int x, int y){
     
-    //Funktion die zurück gibt ob das gesuchte pattern bzw Substr 1 lex. kleiner ist als Text      bzw Substr2
-    // Falls x != 0 wird im Str2 erst ab stelle x gesucht
+	//FEHLENDE FÄLLE: pattern eins länger als nis dahin identes pattern
+	//				  
+	
+    //Funktion die zurück gibt ob das gesuchte pattern bzw Substr 1 lex. kleiner gleich ist als Text      bzw Substr2
+    // Falls y != 0 wird im Str2 erst ab stelle y gesucht
+	//Falls x!= 0 ist pat der zu durchsuende text und text das pattern. Text wird ann ab stelle x durchsucht
     if(x==0 && y==0){
         for(int i = 0; i < pat.size();++i){
             if(pat[i]<=text[i]){
                 if(pat[i]==text[i]){
-                    continue;
+					if(i == pat.size()-1){
+						if(text[i+1]!=*(text.end()-1)){
+							return true;
+						}
+					}                    
+					continue;
                 }else{
                     return true;
                 }   
@@ -49,6 +57,11 @@ bool wholeComp(const std::string pat, const std::string text, int x, int y){   /
         for(int i = 0; i<pat.size();++i){
             if(pat[i+x]<=text[(y+i)]){
                 if(pat[i+x]==text[(y+i)]){
+					if(i == pat.size()-1){
+						if(text[i+1]!=*(text.end()-1)){
+							return true;
+						}
+					}
                     continue;
                 }else{
                     return true;
@@ -59,6 +72,20 @@ bool wholeComp(const std::string pat, const std::string text, int x, int y){   /
         }
     }
 
+    return true;
+}
+
+int isPrefix(std::string query, std::string text, int pos){
+	int count = 0;	
+	for(uint32_t i = 0; i<query.size();++i){
+		if(query[i]==text[pos+i]){
+			count +=1;			
+			continue;
+		}else{
+			return count;
+		}
+	}
+	return count;
 }
 
 void construct(std::vector<uint32_t>& sa, const std::string& text){
@@ -72,7 +99,7 @@ void construct(std::vector<uint32_t>& sa, const std::string& text){
     	sa.resize(text.length());
 		size_t pos = 0;
 		std::generate(sa.begin(), sa.end(), [&pos]() { return pos++; });
-		std::sort(sa.begin(), sa.end(), [text](uint32_t x, uint32_t y) {  //lambda, aber bitte nur auf den Text referenzieren, um bei großen strings nicht aus dem speicher zu fliessen...
+		std::sort(sa.begin(), sa.end(), [text](uint32_t x, uint32_t y) {
 			return wholeComp(text,text,x,y);
 		});
 
@@ -82,7 +109,11 @@ void construct(std::vector<uint32_t>& sa, const std::string& text){
 
 void find(const std::string& query, const std::vector<uint32_t>& sa, const std::string& text, std::vector<uint32_t>& hits){
 
-    //Fehlerabfang    
+    //Fehlerabfang
+   
+    //std::cout<<comp(query,text,3);
+	std::cout<<"SA1: "<<std::endl;
+    
     if(query == ""){
 	    std::cout << "Ein Leeres Pattern könnte überall sein..." << std::endl;
 	    return;
@@ -93,58 +124,91 @@ void find(const std::string& query, const std::vector<uint32_t>& sa, const std::
     }
     
     hits = {};
-    uint32_t lp = 0;
+    
+    uint32_t left = 0;
     uint32_t rp = sa.size()-1;
     uint32_t l = 0;
     uint32_t r = sa.size()-1;
-    uint32_t m = (l+r)/2;
+    uint32_t m = 0;
+	bool test = false;
 
 
-//Searching lp via binary search 
-    if(wholeComp(query, text, 0, sa[0])){
-        lp = 0;
-        std::cout << "IF 1 \n";
-    }else if(wholeComp(query, text, 0, (sa[sa.size()-1]))){
-            lp = text.length();
-    }else{
-        //(l,r)=(1,n) is definded above.
-        std::cout << "Das hier ist der Beginn der while ...\n";
-        while(l<r){
-            m = (l+r)/2; //is it really ceiled? i thought, cpp wound floor the result of a division over to unsigned ints.. 
-            if(wholeComp(query, text, 0, m)){
-                r=m;
-            }else{
-                l=m;
-            }
-        }
-        lp=r;
-    }
+    //Berechnung der Linken Grenze Lp via Binärsuche
 
-std::cout << "\nDas hier ist L: " << l << std::endl;
-std::cout << "\nDas hier ist R: " << r << std::endl;
+    while(!test){
+	uint32_t dist = r-l;
+	if( dist == 0){
+	std::cout<<"IF 1 LP L: "<< l <<std::endl;
+	std::cout<< "R: "<<r <<std::endl;
+		left = l;
+		test = true;
+	}else{
+		m = (l+r)/2;
+		if(!wholeComp(query,text,0,sa[m])){
+			if((query.size()==isPrefix(query,text,sa[m]))){
+				l=m;
+			}else{			
+				l = m+1;
+				std::cout<<"IF 2 LP L: "<< l <<std::endl;
+				std::cout<< "R: "<<r <<std::endl;
+			}
+		}else{
+				std::cout<<"ELSE 2 LP L: "<< l <<std::endl;
+	std::cout<< "R1: "<<r <<std::endl;
+			r = m;
+			std::cout<< "R2: "<<r <<std::endl;
+		}
+	}
+}
 
-l = 0;
-r = sa.size()-1;
-m=0;
-//Searching rp via binary search 
-    if(!wholeComp(query, text, 0, sa[0])){
-        rp = text.length()-1;
-        std::cout << "IF 1 von rp-border \n";
-    }else if(wholeComp(query, text, 0, (sa[sa.size()-1]))){
-            rp = 0;
-    }else{
-        //(l,r)=(1,n) is definded above.
-        std::cout << "Das hier ist der Beginn der while ...\n";
-        while(l<r){
-            m = (l+r)/2; //is it really ceiled? thought, cpp wound floor the result of a division over to unsigned ints.. 
-            if(!wholeComp(query, text, 0, m)){
-                l=m;
-            }else{
-                r=m;
-            }
-        }
-        rp=l;
-    }
-    std::cout << "\nDas hier ist R: " << r << std::endl;
-    std::cout << "\nDas hier ist L: " << l << std::endl;
+
+	std::cout<<"L: "<< l <<std::endl;
+	std::cout<< "R: "<<r <<std::endl;
+	std::cout<< "LEFT BORDER: " << left <<std::endl;
+
+	std::cout<<"POST LP PRÄ RP"<<std::endl;
+    //Berechnung der rechten Grenze rp
+	uint32_t right = 0;
+	l = 0;
+	m = 0;
+	r = sa.size()-1;
+	int tLen = text.size()-1;
+	int qLen = query.size()-1;
+	test = false;
+
+	while(!test){
+		uint32_t dist = r-l;
+		if( dist == 0){
+			right = l;
+			test = true;
+		}else{
+			m = (l+r)/2;
+			//std::string txt = (text.substr(m));
+			//IsPrefix prüft ob txt nicht kürzer als query
+			//und 
+			//bool isPrefix = !((tLen-m)< qLen) && 
+			//bool isPrefix = !((txt.length() < query.length()) && !txt.compare(0, query.length(), query));
+			if(!(wholeComp(query,text,0,sa[m]))|| !(query.size()==isPrefix(query,text,sa[m]))){
+				l = m+1;
+			}else{
+				r = m;
+			}
+		}
+	}
+
+
+	std::cout<<"POST RP"<<std::endl;
+	std::cout<<l <<std::endl;
+	std::cout<<r <<std::endl;
+	std::cout<<"RICHT BORDER: " << right <<std::endl;
+	
+    
+    
+
+    
+
+    
+    
+    
+    
 }

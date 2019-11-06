0f27be84a36eb96d706804ce89fef08464c92eba
maximilian45 <maximilian45@mi.fu-berlin.de>
2019-11-06 23:19:57 +0000

    Replace aufgabe2_main.cpp

--- aufgabe2/aufgabe2_main.cpp
+++ aufgabe2/aufgabe2_main.cpp
@@ -7,23 +7,31 @@
 int main(int argc, char* argv[]){
 
     std::vector<uint32_t> sa;
-	std::vector<uint32_t> hits; 
+	std::vector<uint32_t> hits = {}; 
 
     if(argc == 1){
         std::cout<<"Unexpected Input!!"<<std::endl;
         return 1;
     }
+    /*
     std::string si ="si";
-std::string mi = "mississippi";
+    std::string mi = "mississippi";
     find(si,sa,mi,hits);
-    return 0;
+    */
 
-}
+    std::string text = argv[1];
+    std::cout << text << std::endl;
+    std::string query = argv[2];
+    std::cout << query << std::endl; 
 
-    /*construct(sa, argv[1]); 
-    std::cout<<sa.size();   
-    
-    for(int i = 0; i<sa.size();i++){
-       // std::cout<<"I: "+ i; 
+    construct(sa, argv[1]); 
+    std::cout<<"\nSuffix-Array.size():\n" << sa.size();   
+    std::cout << "\nArray:"<< std::endl;
+    for(int i = 0; i<sa.size();++i){
         std::cout <<  sa[i]<< std::endl;
-    }*/
+    }
+    find(query, sa, text, hits);
+
+
+    return 0;
+}

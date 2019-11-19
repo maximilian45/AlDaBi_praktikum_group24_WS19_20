00e8fddd5819eb36c54a55e0efc8f8c9f9a856fc
acedon <acedon@mi.fu-berlin.de>
2019-11-19 15:54:19 +0000

    Upload New File

---
+++ aufgabe3/aufgabe3_main.cpp
@@ -0,0 +1,22 @@
+#include "Alignment.hpp"
+#include <string>
+
+int main(int argc, char* argv[]){
+    
+    Alignment test{"test1","test"};
+    //
+    test.compute(3,-1,-1,false);
+    
+    if(argc != 5){
+        
+        
+        
+        return 0;
+    }else{
+        //Default Werte für match, mismatch und gap benutzen
+    }
+    
+    
+    
+    return 1;
+}

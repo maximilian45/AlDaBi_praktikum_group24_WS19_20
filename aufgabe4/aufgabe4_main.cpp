ea16e4c0a7856ddc068c964b5db0f0e276e88369
acedon <acedon@mi.fu-berlin.de>
2019-12-03 19:22:47 +0000

    Replace aufgabe4_main.cpp

--- aufgabe4/aufgabe4_main.cpp
+++ aufgabe4/aufgabe4_main.cpp
@@ -16,17 +16,36 @@ int main(int argc, char* argv[]){
         
         try{
         matrix.load(argv[2]);
-        matrix.print();
+        //matrix.print();
         }
         catch(IOException){
             //
             std::cout << "Matrix konnte nicht geladen werden. Bitte Eingabe überprüfen!"<<std::endl;
             return 1;
         }
+        std::string temp = argv[1];
+        if(temp.size() < atoi(argv[3])){
+            return 1;
+        }
+        
         BLAST_Neighborhood test;
-        int startZeit = omp_get_wtime();
-        test.generateNeighborhood(argv[1],matrix,atoi(argv[3]),atoi(argv[4]),atoi(argv[5]));
-        int endZeit = omp_get_wtime() - startZeit;
+        double startZeit = omp_get_wtime();
+        
+        //std::cout << "START "<< startZeit<<std::endl;
+        std::vector<NHResult> result = test.generateNeighborhood(argv[1],matrix,atoi(argv[3]),atoi(argv[4]),atoi(argv[5]));
+        double endZeit = omp_get_wtime() - startZeit;
+        
+        for(int i = 0; i< result.size();++i){
+            std::cout << result[i].infix << ": ";
+            for(int j = 0; j < result[i].neighbours.size() ; ++j){
+                std::cout << "(" << result[i].neighbours[j].first << ", " << result[i].neighbours[j].second << ") ";
+            }
+            std::cout << std::endl;
+        }
+        
+        std::cout << "TIME: " << endZeit<<std::endl;
+    result.clear();
     }
+    
     return 0;
 }

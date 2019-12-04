df286edf14002550c6b5b1de3d302a5e2db4a91d
maximilian45 <maximilian45@mi.fu-berlin.de>
2019-12-04 17:41:08 +0000

    Replace aufgabe4_main.cpp

--- aufgabe4/aufgabe4_main.cpp
+++ aufgabe4/aufgabe4_main.cpp
@@ -8,7 +8,7 @@
 int main(int argc, char* argv[]){
     
     if(argc != 6){
-        std::cout <<argc<<"Fehlerhafte Eingabe. Bitte folgende Argumente Übergeben: Sequenz, Matrixname, Querylength,Treshhold, Thredanzahl"<< std::endl;
+        std::cout << argc << "Fehlerhafte Eingabe. Bitte folgende Argumente Übergeben: Sequenz, Matrixname, Querylength,Treshhold, Thredanzahl"<< std::endl;
         return 1;
     }
     else{
@@ -16,10 +16,8 @@ int main(int argc, char* argv[]){
         
         try{
         matrix.load(argv[2]);
-        //matrix.print();
         }
         catch(IOException){
-            //
             std::cout << "Matrix konnte nicht geladen werden. Bitte Eingabe überprüfen!"<<std::endl;
             return 1;
         }
@@ -31,7 +29,7 @@ int main(int argc, char* argv[]){
         BLAST_Neighborhood test;
         double startZeit = omp_get_wtime();
         
-        //std::cout << "START "<< startZeit<<std::endl;
+        std::cout << "START "<< startZeit<<std::endl;
         std::vector<NHResult> result = test.generateNeighborhood(argv[1],matrix,atoi(argv[3]),atoi(argv[4]),atoi(argv[5]));
         double endZeit = omp_get_wtime() - startZeit;
         
@@ -44,7 +42,7 @@ int main(int argc, char* argv[]){
         }
         
         std::cout << "TIME: " << endZeit<<std::endl;
-    result.clear();
+        result.clear();
     }
     
     return 0;
